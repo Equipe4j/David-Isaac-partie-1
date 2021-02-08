5a2b9fd0ef8d3cd557f4f1a5057b45103b423682
David Elbaz <david.Elbaz@hotmail.Fr>
2021-02-08 10:12:56 +0100

    Partie 1 Isaac/David

---
+++ src/Tools/IMetier.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tools;
+
+import Entity.Exerce;
+import Entity.Laboratoire;
+import Entity.Region;
+import Entity.Secteur;
+import Entity.User;
+import Entity.Visiteur;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC
+ */
+public interface IMetier {
+    public User GetUnUser(String login, String mdp);
+    public ArrayList<Visiteur> GetAllVisiteur();
+    public void InsererVisiteur(int num,String nom, String prenom,String Adresse, String Cp, String Ville,String date,int secteur,int labo,String image);
+    public void ModifierVisiteur(int mat,String nom, String prenom,String Adresse, String Cp, String Ville,String date,int secteur,int labo,String image);
+    public String rechercheVisiteur(String Nom);
+    public ArrayList<Region> GetAllRegions();
+    public void InsererRegions(String Region, String Code,String nom);
+    public void ModifierRegions(String Region, String Code);
+    public ArrayList<Secteur> getlesZones();
+    public ArrayList<Laboratoire>getLesLaboratoires();
+    public int getLabCode(String Nom);
+    public int getSecCode(String nom);
+    public ArrayList<Exerce> GetAllTravaille();
+    public int GetLastMatricule();
+    public String nom(String nom);
+    public ArrayList<Visiteur> nomCherche(String nom);
+    public String getNomLabCode(int labcod);
+    public  String getNomSecCode(int secCode);
+    
+    
+}
